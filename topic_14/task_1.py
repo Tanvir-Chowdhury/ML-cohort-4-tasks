c5daeec326176f323c923c824ec40b5c711958c0
Tanvir-Chowdhury <tanvirvlogger@gmail.com>
2025-07-01 11:04:12 +0600

    Topic 14 tasks done

---
+++ topic_14/task_1.py
@@ -0,0 +1,10 @@
+def NID(name, id, age, gender, address, phone):
+    
+    print("Name:", name)
+    print("ID:", id)
+    print("Age:", age)
+    print("Gender: ", gender)
+    print("Address:", address)
+    print("Phone:", phone)
+    
+NID("Tanvir", 123, 24, "Male", "Dhaka", "01644916069")
